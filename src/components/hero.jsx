9c6561b9955016fbe5593d852ec5bf6f3daeeb60
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-07-04 08:57:29 +0530

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,28 +1,36 @@
 import { Link } from "react-router-dom";
 export const Hero = () => {
   return (
-    <div className="relative h-[80vh] w-full overflow-hidden bg-[url('/hero.jpeg')] bg-contain bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center px-4 ">
+    <div className="relative sm:h-[80vh] h-[50vh] w-full overflow-hidden bg-[url('/hero.jpeg')] bg-contain bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center px-4 ">
       
       {/* Black overlay */}
       <div className="absolute inset-0 bg-black/50"></div>
 
       {/* Text content */}
-      <h1 className="relative text-sm sm:text-2xl md:text-3xl font-bold text-cyan-200 text-center">
+      <h1 className="relative text-sm mt-17 sm:mt-30 sm:text-2xl md:text-3xl font-bold text-cyan-200 text-center">
         Empowering Your Digital Vision
       </h1>
-      <p className="relative mt-3 text-sm sm:text-base md:text-lg text-cyan-200 text-center">
+      <p className="relative sm:mt-3 mt-2 text-sm sm:text-base md:text-lg text-cyan-200 text-center">
         Website | Software | Branding
       </p>
 
       {/* Buttons */}
-      <div className="relative mt-6 flex space-x-4">
-        <Link to="/form" className="bg-cyan-500 text-white px-5 py-2 rounded-lg font-medium hover:bg-cyan-600 transition">
-          Get Started
-        </Link>
-        <a href="tel:+919334167296" className="bg-white text-cyan-600 px-5 py-2 rounded-lg font-medium hover:bg-cyan-100 transition">
-          Contact Us
-        </a>
-      </div>
+     <div className="relative mt-3 sm:mt-6 flex space-x-2 sm:space-x-4">
+  <Link
+    to="/form"
+    className="bg-cyan-500 text-white px-2 py-0.5 sm:px-5 sm:py-2 rounded-md font-medium hover:bg-cyan-600 transition text-xs sm:text-base"
+  >
+    Get Started
+  </Link>
+  <a
+    href="tel:+919334167296"
+    className="bg-white text-cyan-600 px-2 py-0.5 sm:px-5 sm:py-2 rounded-md font-medium hover:bg-cyan-100 transition text-xs sm:text-base"
+  >
+    Contact Us
+  </a>
+</div>
+
+
       
     </div>
   );
